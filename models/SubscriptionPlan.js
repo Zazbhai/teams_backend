61dc85beb1a65e08c6379ca0d775c6d5786eba2e
Zaz Yagmi <96649732+Zazbhai@users.noreply.github.com>
2026-07-14 18:08:58 +0530

    Fix SubscriptionPlan price type to String

--- models/SubscriptionPlan.js
+++ models/SubscriptionPlan.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const subscriptionPlanSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  price: { type: Number, required: true },
+  price: { type: String, required: true },
   description: { type: String },
   duration_days: { type: Number, required: true }
 });
